f8e41186befe0c0c1f704529e78d1115de4caa0e
TM <tm@macadmin.ru>
2015-02-15 21:59:53 +0300

    lec 01

---
+++ spec/sicp/core_spec.clj
@@ -0,0 +1,7 @@
+(ns sicp.core-spec
+  (:require [speclj.core :refer :all]
+            [sicp.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/sicp/core.clj
@@ -0,0 +1,40 @@
+(ns sicp.core)
+
+; Lection 01
+
+(defn square [x]
+  (* x x))
+
+;(defn sum-of-squares [x y]
+;  (+ (square x) (square y)))
+
+(defn abs [x]
+  (if (< x 0)
+   (- x)
+   x))
+
+
+(defn average [x y]
+  (/ (+ x y) 2))
+
+(defn good-enough? [guess x delta]
+  (<
+    (/
+      (abs (-
+             (square guess)
+             x))
+      x)
+    delta))
+
+(defn improve [guess x]
+  (average guess (/ x guess)))
+
+(defn sqrt-iter [guess x delta]
+  (if (good-enough? guess x delta)
+    guess
+    (sqrt-iter (improve guess x) x delta)))
+
+(defn sqrt [x]
+  (sqrt-iter 1.0 x 0.0001))
+
+;(sgrt 2)
